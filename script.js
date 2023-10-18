2ee8b433c3f5da12ab3cce5849b75e0cb31a0f10
afyaruro <andresyaruro20@gmail.com>
2023-10-18 13:22:26 -0500

    nuevos cambios

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-
 $(document).ready(function() {
-    $('#file-upload').on('change', function () {
-        var fileDetails = $('#file-details');
-        var fileInput = $(this)[0];
-        var fileName = fileInput.files[0].name;
-        var fileSize = fileInput.files[0].size;
-        if (this.files.length > 0) {
-            $('#file-name span').text(fileName);
-            $('#file-size span').text(fileSize);
-            fileDetails.css('display', 'block');
-        }else{
-            fileDetails.css('display', 'none');
-        }
-    });
 
     // Evento para cargar un archivo
     $('#submit-button').on('click', function () {
@@ -23,15 +9,12 @@ $(document).ready(function() {
             formData.append('file', fileInput);
 
             $.ajax({
-                url: 'http://balanceadorcargaextremelab-1823199642.us-east-1.elb.amazonaws.com/upload',
+                url: 'http://elbback-1830897251.us-east-1.elb.amazonaws.com/upload',
                 type: 'POST',
                 data: formData,
                 processData: false,
                 contentType: false,
                 success: function (data) {
-                    // Aquí puedes manejar la respuesta si es necesario
-                    var fileDetails = $('#file-details');
-                    fileDetails.css('display', 'none');
                 }
             });
         } else {
@@ -42,7 +25,7 @@ $(document).ready(function() {
     // Evento para actualizar la tabla de archivos
    $('#update-button').on('click', function () {
     $.ajax({
-        url: 'http://balanceadorcargaextremelab-1823199642.us-east-1.elb.amazonaws.com/list',
+        url: 'http://elbback-1830897251.us-east-1.elb.amazonaws.com/list',
         type: 'GET',
         success: function (data) {
             // Limpiar la tabla antes de actualizar
